3c8aeaddb4144c5880887e82d547c63af0ac9665
Sujit Srk <sujit2050@hotmail.com>
2014-06-22 21:22:53 +0530

    Changes commited

--- README.md
+++ README.md
@@ -11,14 +11,24 @@ Steps are as follows
 
 1. Reads the test and training data sets (x_test/x_train) from their source directories and merges them as one dataset, mergedSet by using rbind
 
+
 2. Reads ”features.txt”  to get the column names of the 561-field vector and assigns the column names to the columns of the merged dataset in Step 1
 
-3. Extracts only those columns that have “mean” or “std” in their column names i.e. fields which are either mean or standard deviations. This extracted data set is stored in another dataset, extractedSet. We will now add Subject and descriptive Activity name as additional columns in this dataset to be used for averaging fields over Subject and descriptive Activity  in the last step.
+3. Extracts only those columns that have “mean” or “std” in their column names i.e. fields which are either mean or standard deviations. This extracted data set is stored in another dataset, extractedSet. 
 
-4. The Activities that are measured is stored in y_test/y_train files as numbers – one number for each reading.  The Activity_labels file contains the descriptive Activity names corresponding to the activity numbers.   The y_test and x_test is merged using rbind to get the complete set of readings - this merged dataset is called mergeActivity. Now the Activity numbers in this merged set is replaced by descriptive names by joining with Activity_labels (using R merge) on activity number (activityID). The resulting table contains both  Activity number and descriptive name. We add the Activity name as a column in the extractedSet  created in Step 3.
 
-5. The subjects that are used in the test is stored in subject_test/subject_train files in the respective directories. Similar to the previous step where we merge the test and train activities and create an additional Activity column in the extractedSet, we merge the test and train subjects (using rbind) and then add them as a new column (Subject) to the extractedSet. Now the extractedSet has the mean/std fields along with the Acitivity names and Subject
+4. The Activities that are measured is stored in y_test/y_train files as numbers – one number for each reading.   The y_test and x_test is merged using rbind to get the complete set of readings - this merged dataset is called mergeActivity. We add the mergectivity set as a column in the extractedSet  created in Step 3. This column is renamed to "ActivityID" to be used later while getting activity names.
+
+5. The subjects that are used in the test is stored in subject_test/subject_train files in the respective directories. Similar to the previous step where we merge the test and train activities and create an additional ActivityID column in the extractedSet, we merge the test and train subjects (using rbind) and then add them as a new column (Subject) to the extractedSet. Now the extractedSet has the mean/std fields along with the Acitivity ID and Subject 
 
 6. The field names to be used in the calculation are stripped of braces () and minus – characters as they create errors in the average function (unless escaped )
 
 7. The average of all mean and standard deviation fields for each Subject+Activity combination is taken by using ddply function that groups over Subject and Activity and applies the mean function the group. It also names the field as avg_<field name>
+
+7.  We will now add  descriptive Activity name  instead of Activity ID. For this the file containig the descriptive Activity names "activity_labels"  is read into a data set called acitivityNames which contains the acitivity ID and name.
+
+8. This activityNames is joined  (using R merge) with the extractedSet on activityID to get the Activity Names into the extractedSet
+
+9. The columns in the extractedSet  are reordered to have subject and activity names as first two columns,  Activity ID is removed and the rest of columns are kept.
+
+10. The final data set is written to a comma separated text file written with a write.table so that it can be read by read.table
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,7 @@
 # This function runs analysis on the raw samsung data and produdes a tidy output based 
-# on transformations described in the README file.
+# This program reads the raw Samsung Data and creates a tidy data set
+# having the average of mean and standard deviation fields for each subject nad activity
+# The details of transformation are in the README file
 
 run_analysis <- function () {
   
@@ -39,44 +41,28 @@ run_analysis <- function () {
   extractedSet <- mergedSet [extractColumns]
   
 
+  # Add the Activity and Subject  to the extracedSet by getting data from the test&train Activity and Subect Data sets
+ 
   
-  
-  # ASSIGNMENT STEP 3: Uses descriptive activity names to name the activities in the data set
-  # This is done by 
-  #  a) reading Activity_labels file  and create a ID-Name mapping 
-  #  b) merge test (y_test) and train (y_test) activity sets 
-  #  c) join (merge) output of step a) and step b) to get descriptive activity names for all tests
-  #  d) Add a column "acivity" containing the descriptive acitivity name in the merged&extracted x_values  
-  
-  # Step a: Get Activity labels from activity_labels.txt file
-  activityNames <- read.table("./activity_labels.txt")
-  
-  # Assign column names as activityID and activityName to merge on activityID later
-  colnames(activityNames) <- c("activityID", "activityName")
-  
+  # Add Actiity Columns (IDs not Names - names are added in the last step ) 
   # Read Test Activity table Data set
   testActivity <- read.table("./test/y_test.txt")
   # Read Train Activity table Data set
   trainActivity <- read.table("./train/y_train.txt")
   
-  # Step b: Merge Test and Train Activity sets
+  # Merge Test and Train Activity sets
   mergeActivity <- rbind (testActivity, trainActivity)
   
-  # Assign column name activityID to join (merge) with acitivityNames table on activityID
-  colnames(mergeActivity) <- c("activityID")
+  # Assign column name activityID to join (merge) later on 
+  colnames(mergeActivity) <- c("ActivityID")
   
-  # Step c: Join (merge )  activitySet and activityNames on activityID to descriptive labels for activities
-  activitySet <- merge (mergeActivity, activityNames, by="activityID")
-   
-  # Step d: Now add this activity name set as a newcolumn to the main merged Dataset with the mean/std x values extracted
-  # (column 2 in the activitySet table above  as column 1 is ID) 
-  extractedSet["Activity"] <- activitySet[2]
-  
+  #  Now add this activityID  set as a newcolumn to the main merged Dataset with the mean/std x values extracted 
+ 
+  extractedSet["ActivityID"] <- mergeActivity
   
   # Do similar merge/join/add column for subjects as it was done for Activity above
   # This is required to find the average of Subject-Acitivity for mean/std fields
-  # Note - the Subject does not have descriptive names so directly use the Subject as numbers
-
+ 
   # Read Test Subject table  
   testSubject <- read.table("./test/subject_test.txt")
   
@@ -87,13 +73,13 @@ run_analysis <- function () {
   mergeSubject <- rbind (testSubject, trainSubject)
   
   # Now add the Subject as a newcolumn to the main merged Dataset with the mean/std x values extracted
-  # It already has descriptive Activity name from previus step
+  # It already has  ActivityID from previus step
   extractedSet["Subject"] <- mergeSubject
   
   
   
   # ASSIGNMENT STEP 4: Appropriately labels the data set with descriptive variable names
-  # Here we are cleaning up labels from of braces () and minus - as it will give error in ddply
+  # Here we are cleaning up labels from of braces () and replacing minus - with underscore _ as otherwise it will give error in ddply
   
   #  Clean 1: Remove () from column names as it will cause error in ddply
   colnames(extractedSet) <- gsub("\\(\\)","",colnames(extractedSet))
@@ -103,11 +89,11 @@ run_analysis <- function () {
   # ASSIGNMENT STEP 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
   
   # Plyr function ddply is used to get the average of Subject and Activity
-  # Note - The extractedSet has Acitivty, Subject and all fields in original data set that has 
+  # Note - The extractedSet has AcitivtyID, Subject and all fields in original data set that has 
   # either mean or std  in its name
  
    
-  cdata <- ddply(extractedSet, c("Subject", "Activity"), summarise, 
+  cdata <- ddply(extractedSet, c("ActivityID", "Subject"), summarise, 
                  avg_tbodyAcc_mean_x = mean(tBodyAcc_mean_X), 
                  avg_tBodyAcc_mean_Y = mean(tBodyAcc_mean_Y),
                  avg_tBodyAcc_mean_Z = mean(tBodyAcc_mean_Z),
@@ -190,6 +176,25 @@ run_analysis <- function () {
               
                  )
   
+  #ASSIGNMENT STEP 3: Get descriptive names for the activity.
+  # This is done here as the previous join was giving some problems!
   
+  # Get Activity labels from activity_labels.txt file
+  activityNames <- read.table("./activity_labels.txt")
+  
+  # Assign column names as ActivityID and activityName to merge on activityID later
+  colnames(activityNames) <- c("ActivityID", "ActivityName")
+  
+  ## Now change activityID to activity name
+  cdata2 <- merge (cdata, activityNames, by="ActivityID", sort=FALSE)
+  
+  ## Rearrange columns to have Subject and ACtivityName as first two columns and then the mean/std columns
+  # and remove ActivityID to create the final clean set
+  
+  cdata3 <- cdata2 [c(2,82,3:81)]
+  
+  # Write the tidy data set  to a text file which is uploaded as part of project 
+  
+  write.table(cdata3, file="cdata3.txt", sep=",", col.names=colnames(cdata3))
   
 }
